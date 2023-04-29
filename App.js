ea3e9590f3886020f5eb5df2e34deb2f7c24a2b2
rogerio <rogerio@pop-os.localdomain>
2023-04-29 17:19:21 -0300

    teste botao

--- App.js
+++ App.js
@@ -4,16 +4,33 @@ import axios from 'axios';
 
 import Button from './components/Button';
 
+const esp8266IPAddress = '192.168.0.13'; // Substitua pelo endereço IP do seu ESP8266
+const url = `http://${esp8266IPAddress}/seuarquivo.html`;
+
 
 const App = () => {
-  const sendMessage = (message) =>{
-    Alert.alert(message)
-        
+  alert("teste")
+  const handleButtonClick = () => {
+    Alert.alert('Botão clicado!', 'Você clicou no botão.', [
+      { text: 'OK' }
+    ]);
   }
+
+  const sendData = async () => {
+    try {
+      const response = await axios.post(url, {
+        data: 'seus dados aqui',
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Olá</Text>
-      <Button label='APERTE AQUI'  onClick={() => sendMessage('Alim')}/>
+      <Button label='APERTE AQUI'  onClick={() => handleButtonClick('Alim')}/>
       <StatusBar style="auto" />
     </View>
   );
